8f3ad97e1150258a54b1c567fdc19fff82d3eb19
Xandromus <xanderrapstine@gmail.com>
2018-05-12 13:16:04 -0500

    added user model

--- app/config/config.js
+++ app/config/config.js
@@ -4,7 +4,7 @@ module.exports = {
     development: {
       username: "root",
       password: null,
-      database: "database_development",
+      database: "wanderbase",
       host: "127.0.0.1",
       dialect: "mysql"
     },
---
+++ app/models/user.js
@@ -0,0 +1,40 @@
+// model export for user table/model
+module.exports = function (sequelize, DataTypes) {
+    var User = sequelize.define("User", {
+      username: {
+        type: DataTypes.STRING,
+        // restrict user name from being entered if it doesn't have a text value
+        allowNull: false,
+        // make sure the user name is between 3 and 15 characters
+        validate: {
+          len: [3, 15]
+        }
+      },
+      email: {
+        type: DataTypes.STRING,
+        // restrict email from being entered if it doesn't have a text value
+        allowNull: false,
+        // make sure email is valid email address
+        validate: {
+            isEmail : true
+        }
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      user_image: {
+        type: DataTypes.STRING,
+        // restrict email from being entered if it doesn't have a text value
+        allowNull: false,
+        validate: {
+            isUrl : true
+        }
+      }
+    }, {
+        // disable timestamps
+        timestamps: false
+      }
+    );
+    return User;
+  };

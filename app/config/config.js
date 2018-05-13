2154341a6dec16559ce42ad9ca6a2d462221fb1d
justbbarnett <justb@inkery.com>
2018-05-13 14:10:48 -0500

    html pages with js, css and other assets

--- app/config/config.js
+++ app/config/config.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 
 module.exports = {
-    development: {
+   development: {
       username: "root",
       password: null,
-      database: "wanderbase",
+      database: "database_development",
       host: "127.0.0.1",
       dialect: "mysql"
     },

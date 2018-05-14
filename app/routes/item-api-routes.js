d534e75e5fa9397f7d536932c1be33263b9ccc06
MollyLJohnson <mollyl.johnson@att.net>
2018-05-13 19:35:34 -0500

    working on separated route files

--- app/routes/item-api-routes.js
+++ app/routes/item-api-routes.js
@@ -3,7 +3,6 @@ var db = require("../models");
 
 module.exports = function(app) {
 
-    ////////----GET ROUTES----////////
     //GET route for getting all the **items**
     app.get("/api/suitcases/items", (req, res) => {
         db.Item.findAll({}).then((dbItem) => {
@@ -12,16 +11,7 @@ module.exports = function(app) {
             res.json(err);
         });
     });
-    //GET route for getting all the **suitcases**
-    app.get("/api/suitcases", (req, res) => {
-        db.Suitcase.findAll({}).then((dbSuitcase) => {
-            res.json(dbSuitcase);
-        }).catch((err) => {
-            res.json(err);
-        });
-    });
 
-    ////////----POST ROUTES----////////
     //POST route for saving a new **item**
     app.post("/api/suitcases/items", (req, res) => {
         db.Item.create({
@@ -33,22 +23,7 @@ module.exports = function(app) {
             res.json(err);
         });
     });
-    //POST route for saving a new **suitcase**
-    app.post("/api/suitcases", (req, res) => {
-        db.Suitcase.create({
-            city: req.body.city,
-            start_date: req.body.start_date,
-            end_date: req.body.end_date,
-            travel_category: req.body.travel_category,
-            items: req.body.items
-        }).then((dbSuitcase) => {
-            res.json(dbSuitcase);
-        }).catch((err) => {
-            res.json(err);
-        });
-    });
 
-    ////////----PUT ROUTES----////////
     //PUT route for updating an **item**
     app.put("/api/suitcases/:itemid", (req, res) => {
         db.Item.update({
@@ -65,7 +40,6 @@ module.exports = function(app) {
             });
     });
 
-    ////////----DELETE ROUTES----////////
     //DELETE route for deleting an **item**
     app.delete("/api/suitcases/:itemid",(req, res) => {
         db.Item.destroy({
---
+++ app/routes/user-api-routes.js
@@ -0,0 +1,8 @@
+var db = require("../models");
+
+module.exports = function(app) {
+    //GET
+    //POST
+    //PUT
+    //DELETE
+};

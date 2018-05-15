faf2fb2f1884bb510419334367fe2f63de5c52e2
evanjosephcode <evanseaman89@gmail.com>
2018-05-14 20:10:55 -0500

    added data-id attr to suitcaseHB and hooked 'this' to pixabay.js

--- app/public/assets/js/pixabay.js
+++ app/public/assets/js/pixabay.js
@@ -1,37 +1,26 @@
 $(document).ready(function () {
+    $(".suitcasePhoto").each(function () {
+        let id = $(this).attr("data-id");
+        var authKey = "8978514-366287692940ef0d26d86e99b";
+        // example entries
+        var city = "Austin";
+        var country = "United States";
+        // separate sp
+        var pixaCity = city.replace(/\s+/g, '+');
+        var pixaCountry = country.replace(/\s+/g, '+');
 
-    // api key
-    var authKey = "8978514-366287692940ef0d26d86e99b"
-    // example entries
-    var city = "Austin";
-    var country = "United States";
+        var queryURL = "https://pixabay.com/api/?key=" + authKey + "&q=" + pixaCity + "+" + pixaCountry + "+skyline&image_type=photo";
 
-    var queryURL = "https://pixabay.com/api/?key=" + authKey + "&q=" + city + country + "skyline&image_type=photo";
-
-    // ex populated queryURL w/o vars to sift through JSON object
-    // https://pixabay.com/api/?key=8978514-366287692940ef0d26d86e99b&q=austin+united+states+skyline&image_type=photo
-
-
-    $.ajax({
-            url: queryURL,
-            method: "GET"
-        })
-
-        .then(function (response) {
-            console.log(queryURL);
-            console.log(response);
-
-            // 1280 px (probably too big to go in suitcase graphic)
-            console.log(response.hits[0].largeImageURL);
-            // 640 px (feels right, even says webformat)
-            console.log(response.hits[0].webformatURL);
-
-            // ****** brainstorming logic to hook to html ****
-            if (response.hits[0].webformatURL) {
-                $(".suitcaseImage").prepend(response.hits[0].webformatURL);
-            } else {
-                $(".suitcaseImage").prepend("/assets/images/placeholder.png");
-            }
-
-        });
+        $.ajax({
+                url: queryURL,
+                method: "GET"
+            })
+            .then(response => {
+                if (response.hits[0].webformatURL) {
+                    $(this).attr("src", response.hits[0].webformatURL);
+                } else {
+                    $(this).attr("src", "/assets/img/bg7.jpg");
+                }
+            });
+    });
 });

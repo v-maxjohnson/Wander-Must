c65b1b71ddf94b86836f0f394a6b02c34b6bd2ac
MollyLJohnson <mollyl.johnson@att.net>
2018-05-15 21:13:07 -0500

    I think I wrote these correctly... ???

--- app/routes/item-api-routes.js
+++ app/routes/item-api-routes.js
@@ -3,17 +3,52 @@ var db = require("../models");
 
 module.exports = function(app) {
 
-    //GET route for getting all the **items**
+    //GET route for getting all the **items** then checking if they already exist before creating new ones
     app.get("/api/items", (req, res) => {
         db.Item.findAll({
+            where : {
+                item_name : req.body.item_name
+            }
+        }).then((repeatdbItem) => {
+            db.Item.create({
+                item_name : req.body.item_name,
+                item_category : req.body.item_category,
+                instances : req.body.instances
+            }, {
+                where : {
+                    $notLike: repeatdbItem
+                }
         }).then((dbItem) => {
-            res.json(dbItem);
+        res.json(dbItem);
+        }).catch((err) => {
+        res.json(err);
+        });
+    });
+
+    //GET route for getting all **items** pertaining to a specific suitcase
+    app.get("api/suitcases/:suitcase_id", (req, res) => {
+        db.Suitcase.findAll({ 
+            include: [ 
+                {
+                    model : db.Item, 
+                    where : {
+                        item_name : req.body.item_name,
+                        item_category : req.body.item_category
+                    }
+                }
+            ]
+        }, {
+            where : {
+                id : req.params.suitcase_id
+            }
+        }).then((dbSuitcase) => {
+            res.json(dbSuitcase);
         }).catch((err) => {
             res.json(err);
         });
     });
 
-    //POST route for saving a new **item** to item
+    //POST route for saving a new **item** to item table
     app.post("/api/items", (req, res) => {
         db.Item.create({
             item_name : req.body.item_name,
@@ -26,52 +61,4 @@ module.exports = function(app) {
         });
     });
 
-    //POST route for saving a new **item** to a suitcase
-    app.post("api/suitcases/:suitcaseId", (req, res) => {
-        db.Suitcase.createItem({
-            item_name : req.body.item_name,
-            item_category : req.body.item_category,
-            instances : req.body.instances
-        }, {
-            where : {
-                id : req.params.suitcaseId
-            }
-        }).then((dbSuitcase) => {
-            res.json(dbSuitcase);
-        }).catch((err) => {
-            res.json(err);
-        });
-    });
-
-    //PUT route for updating an **item** in a suitcase
-    app.put("/api/suitcases/:itemId", (req, res) => {
-        db.Suitcase.updateItem({
-            item_name : req.body.item_name,
-            item_category : req.body.item_category,
-            instances : req.body.instances
-        }, {
-            where : {
-                id : req.params.itemId
-            }
-        }).then((dbItem) => {
-                res.json(dbItem);
-        }).catch((err) => {
-                res.json(err);
-        });
-    });
-
-    //***********We don't want to delete an item from the Item table ever, do we??*********//
-
-    //DELETE route for deleting an **item** from a suitcase
-    app.delete("/api/suitcases/:itemiId", (req, res) => {
-        db.Suitcase.destroyItem({
-            where : {
-                id : req.params.itemId
-            }
-        }).then((dbSuitcase) => {
-            res.json(dbSuitcase);
-        }).catch((err) => {
-            res.json(err);
-        });
-    });
 };

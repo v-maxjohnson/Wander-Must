402d733cd233d8ab7cd935940ff4d682f9a5332c
MollyLJohnson <mollyl.johnson@att.net>
2018-05-16 20:22:43 -0500

    changing some 'item' routes to 'suitcase' routes

--- app/routes/item-api-routes.js
+++ app/routes/item-api-routes.js
@@ -3,51 +3,6 @@ var db = require("../models");
 
 module.exports = function(app) {
 
-    //GET route for getting all the **items** then checking if they already exist before creating new ones
-    app.get("/api/items", (req, res) => {
-        db.Item.findAll({
-            where : {
-                item_name : req.body.item_name
-            }
-        }).then((repeatdbItem) => {
-            db.Item.create({
-                item_name : req.body.item_name,
-                item_category : req.body.item_category,
-                instances : req.body.instances
-            }, {
-                where : {
-                    $notLike: repeatdbItem
-                }
-        }).then((dbItem) => {
-        res.json(dbItem);
-        }).catch((err) => {
-        res.json(err);
-        });
-    });
-
-    //GET route for getting all **items** pertaining to a specific suitcase
-    app.get("api/suitcases/:suitcase_id", (req, res) => {
-        db.Suitcase.findAll({ 
-            include: [ 
-                {
-                    model : db.Item, 
-                    where : {
-                        item_name : req.body.item_name,
-                        item_category : req.body.item_category
-                    }
-                }
-            ]
-        }, {
-            where : {
-                id : req.params.suitcase_id
-            }
-        }).then((dbSuitcase) => {
-            res.json(dbSuitcase);
-        }).catch((err) => {
-            res.json(err);
-        });
-    });
-
     //POST route for saving a new **item** to item table
     app.post("/api/items", (req, res) => {
         db.Item.create({

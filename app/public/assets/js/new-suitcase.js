18aae6aad0283ccadeb0ecc26653b3904e542053
Xandromus <xanderrapstine@gmail.com>
2018-05-18 23:46:50 -0500

    fixed modal and got click handler working on new suitcase

---
+++ app/public/assets/js/new-suitcase.js
@@ -0,0 +1,30 @@
+
+
+// When user clicks new-suitcase-btn
+$("#new-suitcase-btn").on("click", function (event) {
+    event.preventDefault();
+
+    var startDate = moment($("#start-date").val(), "MM-DD-YYYY");
+    var endDate = moment($("#end-date").val(), "MM-DD-YYYY");
+
+    var newLocale = {
+        locale_name: $("#suitcase-city").val().trim().toLowerCase()
+    }
+
+    var newSuitcase = {
+        start_date: startDate._i,
+        end_date: endDate._i,
+        travel_category: $("#travelselect").val().trim()
+    };
+
+    console.log(newLocale, newSuitcase);
+
+    // Send an AJAX POST-request with jQuery
+    $.post("/api/suitcases", newLocale, newSuitcase)
+        // On success, run the following code
+        .then(function () {
+            // window.location.href = "/search/:locale";
+        });
+
+    
+});

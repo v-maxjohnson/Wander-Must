d124e98135bcfb3aad6f617a7fa355f7b77b914a
Wayne Baylor <wcsbaylor@gmail.com>
2018-05-19 00:14:59 -0500

    updated passport sign in to redirect to user profile on login

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -13,33 +13,35 @@
 var express = require("express");
 
 // passport is necessary for auth and is thus passed in this function
-module.exports = function(app, passport) {
-    app.get("/signup", function(req, res) {
+module.exports = function (app, passport) {
+    app.get("/signup", function (req, res) {
         res.render("signup")
     });
 
-    app.get("/index", function(req, res) {
+    app.get("/index", function (req, res) {
         res.render("index")
     });
 
-    app.get("/logout", function(req, res) {
-        req.session.destroy(function(err) {
+    app.get("/logout", function (req, res) {
+        req.session.destroy(function (err) {
             res.redirect("/")
         })
     });
 
-    app.get("/profile", isLoggedIn, function(req, res) {
+    app.get("/profile", isLoggedIn, function (req, res) {
         res.render("profile");
     });
 
     app.get("/auth/twitter", passport.authenticate("twitter"));
 
-    app.get('/auth/twitter/return', 
-    passport.authenticate('twitter', { failureRedirect: '/index' }),
-    function(req, res) {
-    // Successful authentication, redirect to customer profile.
-    res.redirect('/profile');
-    });
+    app.get('/auth/twitter/return',
+        passport.authenticate('twitter', {
+            failureRedirect: '/index'
+        }),
+        function (req, res) {
+            // Successful authentication, redirect to customer profile.
+            res.redirect('/profile');
+        });
 
     // route for signing up aka applying
     // the strategy for our /signup route
@@ -47,15 +49,20 @@ module.exports = function(app, passport) {
         successRedirect: "/profile",
         //the only way this failureRedirect is triggered is if there is something thrown on the front end
         failureRedirect: "/signup"
-    }
-    ));
-
-    app.post("/index", passport.authenticate("local-signin", {
-
-        successRedirect: "/profile",
-
-        failureRedirect: "/index"
     }));
+
+    app.post("/index", function (req, res, next) {
+        passport.authenticate("local-signin", function (err, user, info) {
+            if (err) {
+                return next(err);
+            }
+            if (user) {
+                return res.redirect("/profile/" + user.id);
+            }
+            console.log(user);
+            console.log(info);
+        })(req, res, next);
+    });
 
     function isLoggedIn(req, res, next) {
         if (req.isAuthenticated())

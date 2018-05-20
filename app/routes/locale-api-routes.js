6ef64a49b4ce4103afc4d4f3f14ae9f973927fc1
MollyLJohnson <mollyl.johnson@att.net>
2018-05-20 11:32:37 -0500

    added a search by locale to get all associated suitcases

--- app/routes/locale-api-routes.js
+++ app/routes/locale-api-routes.js
@@ -3,6 +3,23 @@ var db = require("../models");
 
 module.exports = function(app) {
 
+    //GET route for getting all the **suitcases** with the same locale_city
+    app.get("/api/locale/:locale_city", (req, res) => {
+        db.Locale.findOne({
+            where : {
+                locale_city : req.params.locale_city
+            }
+        }).then(localeResult => {
+            db.Suitcase.findAll({
+                where : {
+                    locale_id : localeResult.id
+                }
+            }).then((dbSuitcases) => {
+                return res.json(dbSuitcases);
+            });
+        });
+    });
+
     //POST routes for creating a new locale in the locale table
     app.post("/api/locale", (req, res) => {
         db.Locale.create({

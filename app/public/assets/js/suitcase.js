92f6babe0c4550d1d2807ff51a3adae63645f5b1
Xandromus <xanderrapstine@gmail.com>
2018-05-20 22:17:36 -0500

    fixed header of suitcases. all data pulls in, but city search link not working

--- app/public/assets/js/suitcase.js
+++ app/public/assets/js/suitcase.js
@@ -1,72 +1,89 @@
-$("#add-item").on("click", function (event) {
-    event.preventDefault();
+$(document).ready(function () {
 
-    // Make a newChirp object
-    var newItem = {
-        item_name: $("#item-name").val().trim(),
-        item_category: $("#item-category").val().trim()
-    };
+    var dateObjOne = $("#suitcase-startDate").text();
+    var dateObjTwo = $("#suitcase-endDate").text();
+    console.log(dateObjOne);
 
-    console.log(newItem);
+    var momentObjOne = moment(dateObjOne);
+    var momentObjTwo = moment(dateObjTwo);
 
-    $.post("/api/items", newChirp)
-        // On success, run the following code
-        .then(function () {
+    var suitcaseStartDate = momentObjOne.format('MMM DD, YYYY');
+    var suitcaseEndDate = momentObjTwo.format('MMM DD, YYYY');
+    $("#suitcase-startDate").text(suitcaseStartDate);
+    $("#suitcase-endDate").text(suitcaseEndDate);
 
-            var row = $("<div>");
-            row.addClass("chirp");
+    $("#add-item").on("click", function (event) {
+        event.preventDefault();
 
-            row.append("<p>" + newChirp.author + " chirped: </p>");
-            row.append("<p>" + newChirp.body + "</p>");
-            row.append("<p>At " + moment(newChirp.created_at).format("h:mma on dddd") + "</p>");
+        // Make a newChirp object
+        var newItem = {
+            item_name: $("#item-name").val().trim(),
+            item_category: $("#item-category").val().trim()
+        };
 
-            $("#chirp-area").prepend(row);
+        console.log(newItem);
 
-        });
+        $.post("/api/items", newChirp)
+            // On success, run the following code
+            .then(function () {
 
-    // Empty each input box by replacing the value with an empty string
-    $("#author").val("");
-    $("#chirp-box").val("");
-});
+                var row = $("<div>");
+                row.addClass("chirp");
 
-var url = window.location.href.split("/");
-var id = url[url.length - 1];
+                row.append("<p>" + newChirp.author + " chirped: </p>");
+                row.append("<p>" + newChirp.body + "</p>");
+                row.append("<p>At " + moment(newChirp.created_at).format("h:mma on dddd") + "</p>");
 
-$.get("/api/suitcase/" + id, function (dbSuitcase) {
-    if (dbSuitcase.Items.length !== 0) {
+                $("#chirp-area").prepend(row);
 
-        for (var i = 0; i < dbSuitcase.Items.length; i++) {
+            });
 
-            var formCheck = $("<div>");
-            formCheck.addClass("form-check offset-1 col-5 col-lg-3");
-            var formLabel = $("<label>");
-            formLabel.addClass("form-check-label").text(dbSuitcase.Items[i].item_name);
-            var formInput = $("<input class='form-check-input' type='checkbox' checked />");
-            var spans = $("<span class='form-check-sign'><span class='check'></span></span>");
+        // Empty each input box by replacing the value with an empty string
+        $("#author").val("");
+        $("#chirp-box").val("");
+    });
+
+    var url = window.location.href.split("/");
+    var id = url[url.length - 1];
+
+    $.get("/api/suitcase/" + id, function (dbSuitcase) {
+        if (dbSuitcase.Items) {
+            if (dbSuitcase.Items.length !== 0) {
+
+                for (var i = 0; i < dbSuitcase.Items.length; i++) {
+
+                    var formCheck = $("<div>");
+                    formCheck.addClass("form-check offset-1 col-5 col-lg-3");
+                    var formLabel = $("<label>");
+                    formLabel.addClass("form-check-label").text(dbSuitcase.Items[i].item_name);
+                    var formInput = $("<input class='form-check-input' type='checkbox' checked />");
+                    var spans = $("<span class='form-check-sign'><span class='check'></span></span>");
 
 
-            formLabel.append(formInput).append(spans);
-            formCheck.append(formLabel);
+                    formLabel.append(formInput).append(spans);
+                    formCheck.append(formLabel);
 
-            switch (dbSuitcase.Items[i].item_category) {
-                case "toiletries":
-                    $("#toiletries").append(formCheck);
-                    break;
-                case "clothing":
-                    $("#clothing").append(formCheck);
-                    break;
-                case "accessories":
-                    $("#accessories").append(formCheck);
-                    break;
-                case "electronics":
-                    $("#electronics").append(formCheck);
-                    return;
-                    break;
+                    switch (dbSuitcase.Items[i].item_category) {
+                        case "toiletries":
+                            $("#toiletries").append(formCheck);
+                            break;
+                        case "clothing":
+                            $("#clothing").append(formCheck);
+                            break;
+                        case "accessories":
+                            $("#accessories").append(formCheck);
+                            break;
+                        case "electronics":
+                            $("#electronics").append(formCheck);
+                            return;
+                            break;
+                    }
+
+
+                }
+
             }
-
-
         }
-
-    }
+    });
 
 });

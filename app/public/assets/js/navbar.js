e336655d34abb93ced7be8ba6fcb669d0515e17b
Wayne Baylor <wcsbaylor@gmail.com>
2018-05-22 03:56:29 -0500

    logging the userId in localStorage upon login / signup

--- app/public/assets/js/navbar.js
+++ app/public/assets/js/navbar.js
@@ -1,4 +1,16 @@
 $(document).ready(function () {
+    
+    var whyMe = window.location.href;
+    
+    if (whyMe.includes("/profile/")) {
+        console.log("Why are you doing this to me")
+        var userId = whyMe.slice(30);
+        console.log(userId);
+        localStorage.setItem("userId", userId);
+    } else {
+        console.log("yeah i know it's a hack but....")
+    }
+
     if (!req.user) {
         $("#login-dropdown").hide();
         $("#profile-link").show();
--- app/routes/auth.js
+++ app/routes/auth.js
@@ -37,56 +37,6 @@ module.exports = function (app, passport) {
         res.render("profile");
     });
 
-    app.get("/auth/google", passport.authenticate("google"));
-
-    app.get('/auth/google/redirect',
-    passport.authenticate('google', {
-        failureRedirect: '/index'
-    }),
-    function (req, res) {
-        // Successful authentication, redirect to customer profile.
-        res.redirect('/profile');
-    });
-
-    app.get("/login/twitter", passport.authenticate("twitter"));
-
-    app.get('/login/twitter/return',
-        passport.authenticate('twitter', {
-
-            failureRedirect: '/index'
-        }),
-        function (req, res) {
-            // Successful authentication, redirect to customer profile.
-            res.redirect('/profile');
-        });
-
-    // route for signing up aka applying
-    // the strategy for our /signup route
-    // app.post("/api/users", passport.authenticate("local-signup", {
-    //     successRedirect: "/profile",
-        //the only way this failureRedirect is triggered is if there is something thrown on the front end
-        // failureRedirect: "/signup"
-    // }));
-
-    // app.post("/api/users", function (req, res, next) {
-    //     passport.authenticate("local-signup", {
-    //         failureRedirect: "/",
-    //         successRedirect: '/profile'
-    //     },
-    //      function (err, user, info) {
-    //         if (err) {
-    //             return next(err);
-    //         }
-
-    //         req.login(user, function(err) {
-    //             console.log(user);
-    //             // if (err) { return next (err); }
-    //             // return user;
-    //             return res.redirect("/profile/" + user.id);
-    //         })
-    //      })(req, res, next);
-    // });
-
     app.get("/authSuccess", function(req, res){
         console.log("/authSuccess route")
         res.redirect("/profile/" + req.user.id)
@@ -99,20 +49,6 @@ module.exports = function (app, passport) {
             console.log("/api/users" + req.body)
         }
     )
-
-
-    app.get("/loggedIn", function(req, res){
-        // res.json(req.user)
-        // res.redirect("/profile/" + req.user.id)
-        // localStorage.setItem("globalId", req.user.id)
-    })
-
-    // app.post(
-    //     "/api/signin/", 
-    //     passport.authenticate("local-signin", { failureRedirect: "/", successRedirect: "/loggedIn" }), 
-    //     function(req, res){
-    //     }
-    // )
 
     app.post("/api/signin", function (req, res, next) {
         passport.authenticate("local-signin", function (err, user, info) {

481394d052cb85859c3d947a0dbcd4aa4bf702cd
MollyLJohnson <mollyl.johnson@att.net>
2018-05-22 11:50:06 -0500

    fixed some formatting and can save user_id into local storage

--- app/public/assets/js/navbar.js
+++ app/public/assets/js/navbar.js
@@ -3,21 +3,24 @@ $(document).ready(function () {
     var whyMe = window.location.href;
     
     if (whyMe.includes("/profile/")) {
-        console.log("Why are you doing this to me")
-        var userId = whyMe.slice(30);
-        console.log(userId);
-        localStorage.setItem("userId", userId);
+        console.log("Why are you doing this to me");
+        var user_id = whyMe.slice(30);
+        console.log(user_id);
+        localStorage.setItem("user_id", user_id);
     } else {
-        console.log("yeah i know it's a hack but....")
+        console.log("yeah i know it's a hack but....");
     }
 
-    if (!req.user) {
+    var user = localStorage.getItem("user_id");
+    
+    if (user) {
         $("#login-dropdown").hide();
         $("#profile-link").show();
+        $("#suitcase-link").show();
     } else {
         $("#login-dropdown").show();
         $("#profile-link").hide();
+        $("#suitcase-link").hide();
     }
-
 
 });

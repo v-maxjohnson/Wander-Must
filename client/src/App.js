30ef0913b934ca202bdd5d86a18f7407cd6e3c8c
Xandromus <xanderrapstine@gmail.com>
2018-06-18 15:46:56 -0500

    added page components

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,22 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <Router>
+        <div className="App">
+          {/* <Navbar /> */}
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/signup" component={Signup} />
+            {/* <Route component={NoMatch} /> */}
+          </Switch>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </Router>
     );
   }
 }

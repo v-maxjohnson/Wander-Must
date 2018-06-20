8658f2c8caa49cd660b64e31d5af861e1d413a0e
Xandromus <xanderrapstine@gmail.com>
2018-06-19 22:08:00 -0500

    set up all routing and got profile and home working

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,11 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import Signup from "./pages/Signup";
+import Account from "./pages/Account";
+import Items from "./pages/Items";
+import Profile from "./pages/Profile";
+import Search from "./pages/Search";
+import Suitcase from "./pages/Suitcase";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import "./App.css";
@@ -15,6 +20,12 @@ class App extends Component {
           <Switch>
             <Route exact path="/" component={Home} />
             <Route exact path="/signup" component={Signup} />
+            <Route exact path="/account" component={Account} />
+            <Route exact path="/items" component={Items} />
+            <Route exact path="/profile/:id" component={Profile} />
+            <Route exact path="/search/:city" component={Search} />
+            <Route exact path="/search/:user" component={Search} />
+            <Route exact path="/suitcase/:id" component={Suitcase} />
             {/* <Route component={NoMatch} /> */}
           </Switch>
           <Footer />
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -10,7 +10,8 @@ import {
   UncontrolledDropdown,
   DropdownToggle,
   DropdownMenu,
-  DropdownItem } from 'reactstrap';
+  DropdownItem
+} from 'reactstrap';
 import "../styles/Navbar.css";
 
 export default class Navibar extends Component {
@@ -30,65 +31,64 @@ export default class Navibar extends Component {
 
   render() {
     return (
-<div>
-        <Navbar className="navbar navbar-transparent navbar-color-on-scroll fixed-top navbar-expand-lg" color-on-scroll="100" id="sectionsNav">
-        <div className="container">
-      <div className="navbar-translate">
-          <NavbarBrand className="navbar-brand wandermust-font"  href="/">Wander-Must</NavbarBrand>
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="navbar-nav ml-auto" navbar>
-            <li id="login-dropdown" className="nav-item dropdown">
-            <a href="#" className="dropdown-toggle nav-link" data-toggle="dropdown">
-              <i className="fa fa-user-circle" title="Profile Page"> </i>
-            </a>
+      <div>
+        <Navbar className="navbar navbar-transparent fixed-top navbar-expand-lg">
+          <div className="container">
+            <NavbarBrand className="navbar-brand wandermust-font" href="/">Wander-Must</NavbarBrand>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className="navbar-nav ml-auto" navbar>
+                <DropdownItem className="nav-item dropdown">
+                  <a href="" className="dropdown-toggle nav-link" data-toggle="dropdown">
+                    <i className="fa fa-user-circle" title="Profile Page"> </i>
+                  </a>
 
-            <div className="dropdown-menu dropdown-with-icons">
+                  <div className="dropdown-menu dropdown-with-icons">
 
-              <form className="p-3" id="signin" name="" method="post" action="api/signin">
-                <label for="email">Email Address</label>
-                <input className="text" name="email" type="text" />
-                <label for="password">Password</label>
-                <input name="password" type="password" />
-                <div className="row">
-                  <div className="col-6">
-                    <input className="btn btn-primary btn-sm mt-3 mx-auto px-3 py-2" type="submit" value="Login" />
+                    <form className="p-3" id="signin" name="" method="post" action="api/signin">
+                      <label for="email">Email Address</label>
+                      <input className="text" name="email" type="text" />
+                      <label for="password">Password</label>
+                      <input name="password" type="password" />
+                      <div className="row">
+                        <div className="col-6">
+                          <input className="btn btn-primary btn-sm mt-3 mx-auto px-3 py-2" type="submit" value="Login" />
+                        </div>
+
+                        <div className="col-6">
+                          <a className="btn btn-sm mt-3 mx-auto px-3 py-2" href="/signup">SignUp</a>
+                        </div>
+                      </div>
+                    </form>
                   </div>
 
-                  <div className="col-6">
-                    <a className="btn btn-sm mt-3 mx-auto px-3 py-2" href="/signup">SignUp</a>
-                  </div>
-                </div>
-              </form>
-            </div>
+                </DropdownItem>
 
-          </li>
+                <NavItem className="nav-item">
+                  <a href="/profile/" id="profile-link-button" className="nav-link">
+                    <i className="fa fa-user-circle" title="Profile Page"> </i>
+                  </a>
 
-          <li id="profile-link" className="nav-item">
-            <a href="/profile/" id="profile-link-button" className="nav-link">
-              <i className="fa fa-user-circle" title="Profile Page"> </i>
-            </a>
+                </NavItem>
 
-          </li>
+                <NavItem className="nav-item">
+                  <span className="suitcase-input" data-toggle="modal" data-target="#suitcase-modal">
+                    <a className="nav-link" data-toggle="tooltip" title="Add new Suitcase" data-placement="middle" data-original-title="Add new suitcase">
+                      <i className="fa fa-suitcase" data-toggle="tooltip" title="Add new Suitcase"> </i>
+                    </a>
+                  </span>
+                </NavItem>
 
-          <li className="nav-item" id="suitcase-link">
-            <span className="suitcase-input" data-toggle="modal" data-target="#suitcase-modal">
-              <a className="nav-link" data-toggle="tooltip" title="Add new Suitcase" data-placement="middle" data-original-title="Add new suitcase">
-                <i className="fa fa-suitcase" data-toggle="tooltip" title="Add new Suitcase"> </i>
-              </a>
-            </span>
-          </li>
+                <NavItem className="nav-item">
+                  <button className="btn btn-primary btn-sm px-3 py-2" id="logout-btn">Logout</button>
+                </NavItem>
+              </Nav>
+            </Collapse>
 
-          <li className="nav-item" id="logout">
-            <button className="btn btn-primary btn-sm px-3 py-2" id="logout-btn">Logout</button>
-          </li>
-            </Nav>
-          </Collapse>
           </div>
-    </div>
         </Navbar>
       </div>
-   
+
     )
   }
 }

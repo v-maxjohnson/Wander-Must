fd75a4c647dde7a10699ae9a85910430e68d9f4f
evanjosephcode <evanseaman89@gmail.com>
2018-06-21 19:31:33 -0500

    added functioning console.logs for inputs

--- app/public/assets/js/Yelp/Yelp.js
+++ app/public/assets/js/Yelp/Yelp.js
@@ -7,7 +7,7 @@ const yelp = require('yelp-fusion');
 const apiKey = 'te8cuUAAywnS6fTyQVVOj1vRJ-ybh4BijgLA2wgVF6Hk3x70QSiymfYF5Day-HTlD1hwZkniAQcj8GSqId4LAdyjCmFBNoJfd5dDncvaV6PgiHXlPHC4fxqiE_QrW3Yx';
 
 if (zipcode) {
-  var zipcode = 78704;
+  var zipcode = 21114;
 } else {
   var zipcode = null;
 }
@@ -16,7 +16,7 @@ if (radiusMiles >= 25) {
  var radiusMiles = 25; 
 } else {
   // need to only accept integers
-  var radiusMiles = 25;
+  var radiusMiles = 2;
 };
 
 var radiusMeters = radiusMiles * 1600;
@@ -24,21 +24,43 @@ var radiusMeters = radiusMiles * 1600;
 
 const searchRequest = {
     // term could be name of a business too / restaurant / food types, etc. 
-  term:'mexican food',
+  term:'restaurant',
     // put in city and locale   
-  location: 'austin, tx+'+zipcode,
+  location: 'annapolis, md' + ' ' + zipcode,
   radius: radiusMeters
   
 };
 
 const client = yelp.client(apiKey);
 
+// client.search(searchRequest).then(response => {
+//   var results = response;
+//   results.forEach((element) => {
+//     console.log(element);
+//   })
+// }) 
+
+
+// working part 
+
 client.search(searchRequest).then(response => {
-  var results = response;
-  results.forEach((element) => {
-    console.log(element);
-  })
-}) 
+  console.log(response.jsonBody.businesses[0]);
+}).catch(e => {
+  console.log(e);
+});
+
+// +~~+~++~+~+~+~+~+~+~+~ actually working part
+
+// client.search(searchRequest).then(response => {
+//   console.log(JSON.stringify(response, null, 4));
+// }).catch(e => {
+//   console.log(e);
+// });
+
+// _+~_!_!_!_!_!!_!_!_ actually working part 
+
+// 
+
   //   console.log(response.jsonBody.businesses[i]);
     
   // }

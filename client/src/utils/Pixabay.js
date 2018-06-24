39fb1776f36c19b29f3040f2fa06f8ff5e01d8d5
evanjosephcode <evanseaman89@gmail.com>
2018-06-24 12:53:59 -0500

    added boiler plate Pixabay in utils dir

---
+++ client/src/utils/Pixabay.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import axios from "axios";
+
+export default class Wunderground extends Component {
+    const authKey = "8978514-366287692940ef0d26d86e99b";
+    var city = this.props.city;
+    var country = this.props.country;
+    
+    var pixaCity = city.replace(/\s_/g, '+');
+    var pixaCountry = country.replace(/\s_/g, '+');
+
+    var queryURL = "https://pixabay.com/api/?key=" + authKey + "&q=" + pixaCity + "+" + pixaCountry + "+skyline&image_type=photo";
+
+    axios.get(queryURL)
+    .then(function (response) {
+        if (response.hits[0].webformatURL) {
+            // need to figure out what to replace this jQuery DOM stuff w for react 
+            // $(this).attr("src", response.hits[0].webformatURL);
+        } else { 
+
+            // $(this).attr("src", "/assets/img/bg7.jpg");
+        }
+
+})
+

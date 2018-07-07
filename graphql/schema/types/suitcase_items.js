3e08475b5c4473825db62ba13dae25e8ae943144
MollyLJohnson <mollyl.johnson@att.net>
2018-07-07 14:03:04 -0500

    got items returning with item amounts on the getSuitcase query

--- graphql/schema/types/suitcase_items.js
+++ graphql/schema/types/suitcase_items.js
@@ -3,20 +3,11 @@ import {
     GraphQLInt
   } from 'graphql';
 
-import suitcaseType from './suitcase';
-import itemType from './item';
-
 export default new GraphQLObjectType({
     name: 'suitcaseItemType',
     fields: () => ({
         item_amount: {
             type: GraphQLInt
-        },
-        Suitcases: {
-            type: GraphQLList(suitcaseType),
-            Items : {
-                type: GraphQLList(itemType)
-            }
         }
     })
 });

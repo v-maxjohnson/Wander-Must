4c5fec784dbc9650758d94ed8b776ea425691603
Xandromus <xanderrapstine@gmail.com>
2018-07-08 15:36:01 -0500

    got username showing in navbar, got wm link in nav linking to home, moved the delete suitcase button into blog component, and added item amounts to quick view modal

--- client/src/components/Blog.js
+++ client/src/components/Blog.js
@@ -7,66 +7,75 @@ export default class Blog extends Component {
         note_title: "",
         notes: ""
     };
-  
-  
+
+
     handleInputChange = (event) => {
         const { name, newvalue } = event.target;
 
         // Set the state for the appropriate input field
         this.setState({
-          [name]: newvalue
+            [name]: newvalue
         });
     };
-    
-  
+
+
     handleFormSubmit = event => {
         event.preventDefault();
-        this.setState({ note_title: "", notes: ""});
+        this.setState({ note_title: "", notes: "" });
     };
-  
-    render() {
-      return (
-        
-            <Form onSubmit={this.setTerm}>
-                <FormGroup row>
-                    <Col sm={1}/>
-                    <Col sm={2}>
-                        <Label for="note_title" >Title</Label>
-                    </Col>
-                    <Col sm={7}>
-                        <Input 
-                            type="text" 
-                            name="note_title" 
-                            value={this.props.note_title} 
-                            newvalue={this.state.note_title} 
-                            onChange={this.handleInputChange}
-                        />
-                    </Col>
-                </FormGroup>
-                <FormGroup row>
-                    <Col sm={1}/>
-                    <Col sm={2}>
-                        <Label for="notes">Body</Label>
-                    </Col>
-                    <Col sm={7}>
-                        <Input 
-                            type="textarea" 
-                            name="notes" 
-                            rows={8}
-                            value={this.props.notes} 
-                            newvalue={this.state.notes} 
-                            onChange={this.handleInputChange} 
-                        />
-                    </Col>
-                </FormGroup>
-                <FormGroup check row>
-                    <Col sm={{ size: 2, offset: 5 }}>
-                    <Button color="primary" onClick={this.handleFormSubmit} >Submit</Button>
-                    </Col>
-                </FormGroup>
-            </Form> 
 
-        
-      )
+    render() {
+        return (
+            <div className="blog">
+                <Form onSubmit={this.setTerm}>
+                    <FormGroup row>
+                        <Col sm={1} />
+                        <Col sm={2}>
+                            <Label for="note_title" >Title</Label>
+                        </Col>
+                        <Col sm={7}>
+                            <Input
+                                type="text"
+                                name="note_title"
+                                value={this.props.note_title}
+                                newvalue={this.state.note_title}
+                                onChange={this.handleInputChange}
+                            />
+                        </Col>
+                    </FormGroup>
+                    <FormGroup row>
+                        <Col sm={1} />
+                        <Col sm={2}>
+                            <Label for="notes">Body</Label>
+                        </Col>
+                        <Col sm={7}>
+                            <Input
+                                type="textarea"
+                                name="notes"
+                                rows={8}
+                                value={this.props.notes}
+                                newvalue={this.state.notes}
+                                onChange={this.handleInputChange}
+                            />
+                        </Col>
+                    </FormGroup>
+                    <FormGroup check row>
+                        <Col sm={{ size: 2, offset: 5 }}>
+                            <Button color="primary" onClick={this.handleFormSubmit} >Submit</Button>
+                        </Col>
+                    </FormGroup>
+                </Form>
+
+                <div className="row">
+                    <div className="col-12 text-center">
+                        {this.props.loggedInUserIdNumber === this.props.suitcaseUserId ? (
+                            <button className="btn btn-primary" onClick={() => { this.props.showConfirmationModal() }}><i className="fa fa-trash mr-2"></i> Delete this suitcase</button>
+                        ) : (<div></div>
+                            )}
+                    </div>
+                </div>
+
+            </div>
+        )
     }
-  }
+}
--- client/src/pages/Account.js
+++ client/src/pages/Account.js
@@ -8,7 +8,7 @@ import gql from "graphql-tag";
 import ApolloClient from 'apollo-boost';
 
 const GET_USER_QUERY = gql`
-query getUser( $id: String! ){
+query getUser( $id: ID ){
   getUser(id: $id) {
     id
     username
@@ -51,7 +51,7 @@ export default class Account extends Component {
       const { name, value } = event.target;
   
       // Set the state for the appropriate input field
-      this.setState.userData({
+      this.setState({
         [name]: value
       });
     };
@@ -139,13 +139,7 @@ export default class Account extends Component {
                           onChange={this.handleInputChange}
                         />
                       </Col>
-                      {/* <Col sm={1}>
-                        <button data-category="email" className="all btn btn-default btn-sm btn-fab btn-round">
-                          <a className="nav-link" data-toggle="tooltip" title="Add new Suitcase" data-placement="middle" data-original-title="Confirm new email.">
-                          <i className="fa fa-check-circle-o" data-toggle="tooltip" title="Confirm new email"> </i>
-                          </a> 
-                        </button>
-                      </Col> */}
+                     
                     </FormGroup>
                     <FormGroup row>
                       <Label for="username" sm={3}>User Name</Label>
@@ -180,30 +174,16 @@ export default class Account extends Component {
                           onChange={this.handlePasswordChange}
                         />
                       </Col>
-                      {/* <Col sm={1}>
-                        <button data-category="password" className="all btn btn-default btn-sm btn-fab btn-round">
-                          <a className="nav-link" data-toggle="tooltip" title="Confirm new password" data-placement="middle" data-original-title="Confirm new password.">
-                            <i className="fa fa-check-circle-o" data-toggle="tooltip" title="Confirm new password"> </i>
-                          </a> 
-                        </button>
-                      </Col> */}
                     </FormGroup>
                     <FormGroup row>
                       <Label for="exampleCheckbox" sm={3}>Gender</Label>
                       <Col sm={9}>
                         <div>
-                          <CustomInput type="radio" name="gender" label="Female" inline />
+                          <CustomInput value="" type="radio" name="gender" label="Female" inline />
                           <CustomInput type="radio" name="gender" label="Male" inline />
                           <CustomInput type="radio" name="gender" label="Beyond Society's Gender Definitions" inline />
                         </div>
                       </Col>
-                      {/* <Col sm={1}>
-                        <button data-category="gender" className="all btn btn-default btn-sm btn-fab btn-round">
-                          <a className="nav-link" data-toggle="tooltip" title="Confirm new gender" data-placement="middle" data-original-title="Confirm new gender.">
-                          <i className="fa fa-check-circle-o" data-toggle="tooltip" title="Confirm new gender"> </i>
-                          </a> 
-                        </button>
-                      </Col> */}
                     </FormGroup>
                     <FormGroup row>
                       <Label for="exampleCustomFileBrowser" sm={3}>Avatar</Label>
@@ -215,13 +195,6 @@ export default class Account extends Component {
                           label="What's your image?" 
                         />
                       </Col>
-                      {/* <Col sm={1}>
-                        <button data-category="avatar" className="all btn btn-default btn-sm btn-fab btn-round">
-                          <a className="nav-link" data-toggle="tooltip" title="Confirm new image" data-placement="middle" data-original-title="Confirm new image">
-                          <i className="fa fa-check-circle-o" data-toggle="tooltip" title="Confirm new image"> </i>
-                          </a> 
-                        </button>
-                      </Col> */}
                     </FormGroup>
                     <FormGroup check row>
                       <Col sm={{ size: 2, offset: 5 }}>
